4cb2740d9bf9a6afa3f96636349841ac035b974a
McFuzzykins <77823726+McFuzzykins@users.noreply.github.com>
2022-11-20 20:25:41 -0700

    scripts
    no highlight

---
+++ Scripts/ISelector.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISelector
+{
+    void Check(Ray ray);
+
+    Transform GetSelection();
+}
---
+++ Scripts/SelectionManager.cs
@@ -0,0 +1,39 @@
+﻿﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectionManager : MonoBehaviour
+{
+    private IRayProvider _rayProvider;
+    private ISelector _selector;
+    private ISelectionResponse _selectionResponse;
+
+    private Transform _curSelection;
+
+    private void Awake()
+    {
+        SceneManager.LoadScene("Environment", LoadSceneMode.Additive);
+        SceneManager.LoadScene("UI", LoadSceneMode.Additive);
+
+        _selectionResponse = GetComponent<ISelectionResponse>();
+        _rayProvider = GetComponent<IRayProvider>();
+        _selector = GetComponent<ISelector>();
+    }
+
+    private void Update()
+    {
+        //Deselection/Selection Response
+        if (_curSelection != null)
+        {
+            _selectionResponse.OnDeselect(_curSelection);
+        }
+
+        _selector.Check(_rayProvider.CreateRay());
+        _curSelection = _selector.GetSelection();
+
+        //Deselection/Selection Response
+        if (_curSelection != null)
+        {
+            _selectionResponse.OnSelect(_curSelection);
+        }
+    }
+}
